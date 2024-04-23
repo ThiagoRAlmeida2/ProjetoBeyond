9c27af040f098201214314a4566ddc833ffb1070
king <129293006+ThiagoRAlmeida2@users.noreply.github.com>
2024-04-23 20:46:12 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<!-- Trecho de HTML -->
+<template>
+  <div id="app">
+  Hello world
+  </div>
+</template>
+
+<!-- Trecho de JS -->
+<script>
+</script>
+
+<!-- Trecho de Css -->
+<style>
+</style>
