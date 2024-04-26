a1c149fafdbde68a2cc6d80672710d1f4bf18c9c
king <129293006+ThiagoRAlmeida2@users.noreply.github.com>
2024-04-26 00:41:15 -0300

    v2.2

--- src/pages/Post.vue
+++ src/pages/Post.vue
@@ -2,67 +2,115 @@
   <v-app>
     <v-main>
       <Header></Header>
-    </v-main>
-    <body>
-    <v-card>
-      <v-toolbar
-          flat
-          color="blue"
-          dark
-      >
-        <v-toolbar-title>Submit a post</v-toolbar-title>
-      </v-toolbar>
-
-      <v-card-text>
-        <v-text-field
-            filled
-            label="Title"
-            value="My new post"
-        ></v-text-field>
-
-        <v-textarea
-            filled
-            label="Text"
-            value="Write your text"
-            class="custom-text"
-        ></v-textarea>
-
-        <v-divider class="my-2"></v-divider>
-
-        <v-item-group multiple>
-          <v-subheader>Tags</v-subheader>
-          <v-item
-              v-for="n in 9"
-              :key="n"
-              v-slot="{ active, toggle }"
-          >
-            <v-chip
-                active-class="blue--text"
-                :input-value="active"
-                @click="toggle"
-            >
-              Tag {{ n }}
-            </v-chip>
-          </v-item>
-        </v-item-group>
-      </v-card-text>
-
-      <v-divider></v-divider>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-            color="success"
-            depressed
+      <v-container fluid>
+        <v-row wrap
+               align="center"
+               gap="5px"
         >
-          Post
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-    </body>
-    <footer>
-      <MainFooter></MainFooter>
-    </footer>
+          <v-col xs="12" sm="6" md="4" :class="['spacing-card']"> <!-- Aplicando classe personalizada para espaçamento -->
+            <v-card class="Card" color="blue" dark max-width="400">
+              <v-toolbar flat color="blue" dark>
+                <v-toolbar-title>Submit a post</v-toolbar-title>
+              </v-toolbar>
+
+              <v-card-text>
+                <!-- Título da postagem -->
+                <v-text-field
+                    filled
+                    label="Title"
+                    v-model="postTitle"
+                ></v-text-field>
+
+                <!-- Texto da postagem -->
+                <v-textarea
+                    filled
+                    label="Text"
+                    v-model="postText"
+                ></v-textarea>
+
+                <!-- Campo para carregar imagem -->
+                <v-file-input
+                    label="Upload image"
+                    v-model="postImage"
+                    accept="image/*"
+                ></v-file-input>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <!-- Botão para postar -->
+                <v-btn
+                    color="success"
+                    depressed
+                    @click="post"
+                >
+                  Post
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+
+          <!-- Exibição das postagens -->
+          <v-col xs="1" sm="3" md="3">
+            <div v-for="(post, index) in posts" :key="index">
+              <v-card
+                  class="post-card"
+                  color="blue"
+                  dark
+                  max-width="400"
+              >
+                <v-card-title>
+                  <v-icon large left>
+                    mdi-post
+                  </v-icon>
+                  <span class="text-h6 font-weight-light">Forum</span>
+                  <v-spacer></v-spacer>
+                  <v-icon
+                      @click="deletePost(index)"
+                  color="withe"
+                  large
+                  >
+                  mdi-close
+                  </v-icon>
+                </v-card-title>
+
+                <v-img
+                    :src="post.image"
+                    class="elevation-6"
+                    height="200px"
+                ></v-img>
+
+                <v-card-text class="text-h5 font-weight-bold">
+                  {{ post.text }}
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-list-item>
+                    <v-list-item-avatar color="grey darken-3">
+                      <v-img
+                          class="elevation-6"
+                      ></v-img>
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                      <v-list-item-title>{{ post.title }}</v-list-item-title>
+                    </v-list-item-content>
+
+                    <v-row align="center" justify="end">
+                      <v-icon class="mr-1">mdi-heart</v-icon>
+                      <span class="subheading mr-2"></span>
+                      <v-icon class="mr-1">mdi-share-variant</v-icon>
+                      <span class="subheading"></span>
+                    </v-row>
+                  </v-list-item>
+                </v-card-actions>
+              </v-card>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <MainFooter></MainFooter>
   </v-app>
 </template>
 
@@ -75,14 +123,49 @@ export default {
     Header,
     MainFooter,
   },
-  data: () => ({
-  }),
+  data() {
+    return {
+      postTitle: "",
+      postText: "",
+      postImage: null,
+      posts: [],
+    };
+  },
+  methods: {
+    post() {
+      if (this.postImage) {
+        const imageSrc = URL.createObjectURL(this.postImage);
+        this.posts.push({
+          title: this.postTitle,
+          text: this.postText,
+          image: imageSrc,
+        });
+      }
+
+      this.postTitle = "";
+      this.postText = "";
+      this.postImage = null;
+    },
+    deletePost(index) {
+      this.posts.splice(index, 1);
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style>
+.Card{
+  text-align: left;
+  justify-content: flex-start;
+  border-radius: 8px;
+}
 
-.custom-text {
-  text-decoration-color: rgba(255, 255, 255, 0.7);
+.spacing-card {
+  padding-right: 6px;
+}
+
+.post-card{
+  margin-right: 5px;
+  margin-bottom: 5px;
 }
 </style>
