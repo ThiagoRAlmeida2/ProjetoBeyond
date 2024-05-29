4a420fc8fc8b6ed2d5d09826df3a051a3438b719
king <129293006+ThiagoRAlmeida2@users.noreply.github.com>
2024-05-29 14:09:40 -0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import Vuetify from "vuetify/lib/framework";
-import router from './routes/router'
-import "vuetify/dist/vuetify.min"
-import { auth } from './service/firebase'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import Vuetify from 'vuetify/lib/framework';
+import router from './routes/router';
+import { auth } from './service/firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
-Vue.use(Vuex)
-Vue.use(Vuetify)
+Vue.use(Vuex);
+Vue.use(Vuetify);
 
 const store = new Vuex.Store({
     state: {
@@ -23,33 +22,32 @@ const store = new Vuex.Store({
     actions: {
         async create({ commit }, payload) {
             const { email, password } = payload;
-            // eslint-disable-next-line no-useless-catch
             try {
                 const result = await createUserWithEmailAndPassword(auth, email, password);
-                console.log("Usuário criado!", result);
-                commit("setUser", result.user); // Adiciona o usuário ao estado após criação
+                console.log('Usuário criado!', result);
+                commit('setUser', result.user);
             } catch (error) {
-                throw error; // Lança o erro para ser tratado no componente
+                throw error;
             }
         },
         async login({ commit }, payload) {
             const { email, password } = payload;
-            // eslint-disable-next-line no-useless-catch
             try {
                 const result = await signInWithEmailAndPassword(auth, email, password);
-                console.log("Usuário logado!", result);
-                commit("setUser", result.user);
+                console.log('Usuário logado!', result);
+                commit('setUser', result.user);
             } catch (error) {
-                throw error; // Lança o erro para ser tratado no componente
+                throw error;
             }
         },
     }
 });
-Vue.config.productionTip = false
 
-new vue({
+Vue.config.productionTip = false;
+
+new Vue({
     vuetify,
     store,
     router,
     render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
